0a63aa7134c5cd5ac427f7f1a59318325cb8887b
Maurizio Cimadamore <maurizio.cimadamore@oracle.com>
2025-08-26 16:40:13 +0100

    Simplify implementation

--- src/java.base/share/classes/jdk/internal/foreign/StructuredSession.java
+++ src/java.base/share/classes/jdk/internal/foreign/StructuredSession.java
@@ -14,12 +14,6 @@ public final class StructuredSession extends MemorySessionImpl {
 
     private final ThreadFlock flock = ThreadFlock.open("Arena$" + this.hashCode());
 
-    private int asyncReleaseCount = 0;
-    private int asyncAcquireCount = 0;
-
-    static final VarHandle ASYNC_RELEASE_COUNT= MhUtil.findVarHandle(MethodHandles.lookup(), "asyncReleaseCount", int.class);
-    static final VarHandle ASYNC_ACQUIRE_COUNT= MhUtil.findVarHandle(MethodHandles.lookup(), "asyncAcquireCount", int.class);
-
     public StructuredSession(Thread owner) {
         super(owner, new ConfinedResourceList());
     }
@@ -44,22 +38,16 @@ public final class StructuredSession extends MemorySessionImpl {
     @Override
     @ForceInline
     public void acquire0() {
+        checkValidState();
         if (Thread.currentThread() == owner) {
             if (acquireCount == MAX_FORKS) {
                 throw tooManyAcquires();
             }
             acquireCount++;
-        } else {
-            // It is possible to end up here in two cases: this session was kept alive by some other confined session
-            // which is implicitly released (in which case the release call comes from the cleaner thread). Or,
-            // this session might be kept alive by a shared session, which means the release call can come from any
-            // thread.
-            int old = (int)ASYNC_ACQUIRE_COUNT.getAndAdd(this, 1);
-            if (old == MAX_FORKS) {
-                ASYNC_ACQUIRE_COUNT.getAndAdd(this, -1);
-                throw tooManyAcquires();
-            }
         }
+        // if the acquiring thread is some other thread nested in this session' flock
+        // we don't need to do anything -- after all we cannot close this session
+        // until all the threads nested in this flock have completed
     }
 
     @Override
@@ -67,13 +55,10 @@ public final class StructuredSession extends MemorySessionImpl {
     public void release0() {
         if (Thread.currentThread() == owner) {
             acquireCount--;
-        } else {
-            // It is possible to end up here in two cases: this session was kept alive by some other confined session
-            // which is implicitly released (in which case the release call comes from the cleaner thread). Or,
-            // this session might be kept alive by a shared session, which means the release call can come from any
-            // thread.
-            ASYNC_RELEASE_COUNT.getAndAdd(this, 1);
         }
+        // if the releasing thread is some other thread nested in this session' flock
+        // we don't need to do anything -- after all we cannot close this session
+        // until all the threads nested in this flock have completed
     }
 
     @Override
@@ -91,10 +76,7 @@ public final class StructuredSession extends MemorySessionImpl {
         if (flock.threads().findAny().isPresent()) {
             throw new IllegalStateException("Cannot close");
         }
-        int asyncReleaseCount = (int)ASYNC_RELEASE_COUNT.getVolatile(this);
-        int asyncAcquireCount = (int)ASYNC_ACQUIRE_COUNT.getVolatile(this);
-        int acquire = acquireCount - asyncReleaseCount + asyncAcquireCount;
-        if (acquire == 0) {
+        if (acquireCount == 0) {
             state = CLOSED;
             flock.close();
         }
