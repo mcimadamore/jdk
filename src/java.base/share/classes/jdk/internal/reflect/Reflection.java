cd165f812a82191a378199f7027ec8d7dd0dcf31
Maurizio Cimadamore <maurizio.cimadamore@oracle.com>
2026-04-22 18:55:00 +0100

    Comment out restricted check Add benchmark

--- src/java.base/share/classes/jdk/internal/reflect/Reflection.java
+++ src/java.base/share/classes/jdk/internal/reflect/Reflection.java
@@ -112,17 +112,17 @@ public class Reflection {
 
     @ForceInline
     public static void ensureNativeAccess(Class<?> currentClass, Class<?> owner, String methodName, boolean jni) {
-        // if there is no caller class, act as if the call came from unnamed module of system class loader
-        Module module = currentClass != null ?
-                currentClass.getModule() :
-                ClassLoader.getSystemClassLoader().getUnnamedModule();
-        class Holder {
-            static final JavaLangAccess JLA = SharedSecrets.getJavaLangAccess();
-        }
-        if (module != null) {
-            // not in init phase
-            Holder.JLA.ensureNativeAccess(module, owner, methodName, currentClass, jni);
-        }
+//        // if there is no caller class, act as if the call came from unnamed module of system class loader
+//        Module module = currentClass != null ?
+//                currentClass.getModule() :
+//                ClassLoader.getSystemClassLoader().getUnnamedModule();
+//        class Holder {
+//            static final JavaLangAccess JLA = SharedSecrets.getJavaLangAccess();
+//        }
+//        if (module != null) {
+//            // not in init phase
+//            Holder.JLA.ensureNativeAccess(module, owner, methodName, currentClass, jni);
+//        }
     }
 
     /**
